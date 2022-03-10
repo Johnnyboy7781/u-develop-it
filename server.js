2fe8d360136a2709ba376b49564d6f22f88d75b2
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-03-10 17:19:59 -0500

    Set up JOIN queries

--- server.js
+++ server.js
@@ -19,7 +19,10 @@ const db = mysql.createConnection(
 
 // GET all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = 'SELECT * FROM candidates';
+    const sql = `SELECT candidates.*, parties.name AS party_name
+                 FROM candidates
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id`;
     db.query(sql, (err, rows) => {
         if (err) {
             res.statusCode(500).json({ error: err.message });
@@ -34,7 +37,11 @@ app.get('/api/candidates', (req, res) => {
 
 // GET single candidate
 app.get('/api/candidates/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name AS party_name
+                 FROM candidates
+                 LEFT JOIN parties
+                 ON candidates.party_id = parties.id
+                 WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
